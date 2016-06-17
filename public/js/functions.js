2efe40d0c2dc8ab43b43270a8dfdc6b3cca194c3
James Hall <llahwj33@gmail.com>
2016-06-16 23:35:36 -0400

    Refactored javascript using HTML5 data- and added minor styling to products page.

--- public/js/functions.js
+++ public/js/functions.js
@@ -7,35 +7,59 @@ var modularWrapper      = document.querySelector('.modular-wrapper');
 var mobileTopBar        = document.querySelector('.mobile-topbar');
 
 
-// Modular Product
+// Mobile Nav
 var panels      = document.querySelectorAll('.panel');
 var mobileLinks = document.querySelectorAll('.mobile-link');
+var innerLinks  = document.querySelectorAll('.mobile-inner-links');
+
 
 // Sale Modal
 var saleModalWrapper = document.querySelector('.sale-modal-wrapper');
 var saleModalRemove  = document.querySelector('.sale-modal-remove');
 
+// Products
+var productsListWrapper = document.querySelector('.products-list-wrapper');
+
+// HTML5 DATA
+var article = document.querySelector('article');
+
 //*******************************************************
 //                   EVENT LISTENERS
 //*******************************************************
 
-mobileMenuBtn.addEventListener('click', toggleMenu);
-saleModalWrapper.addEventListener('click', function() {
+//*********************************************************
+//                 Index Page Events ONLY
+//*********************************************************
 
-});
+if ( article.dataset.title === 'Index' ) {
+  mobileMenuBtn.addEventListener('click', toggleMenuIndex);
+  saleModalRemove.addEventListener('click', removeModal);
 
-saleModalRemove.addEventListener('click', removeModal);
+  window.addEventListener('load', function() {
+      saleModalWrapper.style.transform = 'translateY(0)';
+  });
+}
+
+//*********************************************************
+//                 Categories Pages Events
+//*********************************************************
+
+if ( article.dataset.title === 'Main-Categories' ) {
+  mobileMenuBtn.addEventListener('click', toggleMenuMCate);
+}
+
+//*********************************************************
+//                 Product Pages Events
+//*********************************************************
+
+if ( article.dataset.title === 'Products-List' ) {
+  mobileMenuBtn.addEventListener('click', toggleMenuProducts);
+}
 
 
-window.addEventListener('load', function() {
-  saleModalWrapper.style.transform = 'translateY(0)';
-})
-
-// panels.addEventListener('click', function() {
-//   panels.forEach(function(pan) {
-//     console.log(pan);
-//   });
-// });
+//*********************************************************
+//                     FOR EACH
+//*********************************************************
 
 panels.forEach(function(pan) {
   pan.addEventListener('click', function() {
@@ -49,20 +73,38 @@ panels.forEach(function(pan) {
 mobileLinks.forEach(function(link) {
   link.addEventListener('click', function() {
     this.classList.toggle('active-link');
+    innerLinks.forEach(function(innerLinkage) {
+      innerLinkage.classList.toggle('active-inner-links');
+    })
   })
 });
+
 
 //*******************************************************
 //                     FUNCTIONS
 //*******************************************************
 
+
+function toggleMenuIndex() {
+  saleModalWrapper.classList.toggle('active-mobile');
+  modularWrapper.classList.toggle('active-mobile');
+  toggleMenu();
+}
+
+function toggleMenuMCate() {
+  modularWrapper.classList.toggle('active-mobile');
+  toggleMenu();
+}
+
+function toggleMenuProducts() {
+  productsListWrapper.classList.toggle('active-mobile');
+  toggleMenu();
+}
+
 function toggleMenu() {
-      body.classList.toggle('active-mobile');
-      modularWrapper.classList.toggle('active-mobile');
-      mobileTopBar.classList.toggle('active-mobile');
-      mobileHeaderWrapper.classList.toggle('active-mobile');
-      saleModalWrapper.classList.toggle('active-mobile');
-      cycleSpans();
+  mobileTopBar.classList.toggle('active-mobile');
+  mobileHeaderWrapper.classList.toggle('active-mobile');
+  cycleSpans();
 }
 
 function cycleSpans() {
