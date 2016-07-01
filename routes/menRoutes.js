65893adef70d11bbfdd5175f090b39b4e9645833
James Hall <llahwj33@gmail.com>
2016-07-01 15:43:24 -0400

    js clean up and changed styling to media queries.

--- routes/menRoutes.js
+++ routes/menRoutes.js
@@ -26,22 +26,22 @@ router.get('/men/shirts', function(req, res) {
     {
       name: 'White Tee',
       price: '10.99',
-      image: 'http://images.beallsflorida.com/is/image/Bealls/ThumbnailImg?$layer_2_src=301-3349-4008-01-yyy&$layer_3_hide=1&$layer_4_hide=1&$layer_5_hide=1&$layer_6_hide=1&$layer_7_hide=1'
+      image: 'http://images.beallsflorida.com/is/image/Bealls/ThumbnailImg?$layer_2_src=470-1458-4765-01-yyy&$layer_3_hide=1&$layer_4_hide=1&$layer_5_hide=1&$layer_6_hide=1&$layer_7_hide=1'
     },
     {
       name: 'White Tee',
       price: '10.99',
-      image: 'http://images.beallsflorida.com/is/image/Bealls/ThumbnailImg?$layer_2_src=301-3349-4008-01-yyy&$layer_3_hide=1&$layer_4_hide=1&$layer_5_hide=1&$layer_6_hide=1&$layer_7_hide=1'
+      image: 'http://images.beallsflorida.com/is/image/Bealls/ThumbnailImg?$layer_2_src=469-7938-7840-45-yyy&$layer_3_hide=1&$layer_4_hide=1&$layer_5_hide=1&$layer_6_hide=1&$layer_7_hide=1'
     },
     {
       name: 'White Tee',
       price: '10.99',
-      image: 'http://images.beallsflorida.com/is/image/Bealls/ThumbnailImg?$layer_2_src=301-3349-4008-01-yyy&$layer_3_hide=1&$layer_4_hide=1&$layer_5_hide=1&$layer_6_hide=1&$layer_7_hide=1'
+      image: 'http://images.beallsflorida.com/is/image/Bealls/ThumbnailImg?$layer_2_src=470-1458-4763-41-yyy&$layer_3_hide=1&$layer_4_hide=1&$layer_5_hide=1&$layer_6_hide=1&$layer_7_hide=1'
     },
     {
       name: 'White Tee',
       price: '10.99',
-      image: 'http://images.beallsflorida.com/is/image/Bealls/ThumbnailImg?$layer_2_src=301-3349-4008-01-yyy&$layer_3_hide=1&$layer_4_hide=1&$layer_5_hide=1&$layer_6_hide=1&$layer_7_hide=1'
+      image: 'http://images.beallsflorida.com/is/image/Bealls/ThumbnailImg?$layer_2_src=323-1602-7311-08-yyy&$layer_3_hide=1&$layer_4_src=Bealls/Bealls%20Exclusive&$layer_4_hide=0&$layer_5_hide=1&$layer_6_hide=1&$layer_7_hide=1'
     }
   ]
 
